762f9a76e1a36b29dd7a7c9892223bebe8117ea7
Artanith <nate.l.allen@gmail.com>
2022-12-19 22:41:16 +0000

    Updated retireAnalysis function to account for updated user inputs on subsequent runs and to add loop variables to properly calculate compounding interest and salary increases

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,4 +1,5 @@
 //Initialize Variables
+var retireValue = 0;
 
 //Social Security Could Be Future Improvement
 
@@ -7,42 +8,48 @@ function grabValue(name){
     return parseFloat(document.getElementById(name).value);
 };
 
-//Grab Values from User Inputs
-var income = grabValue("income");
-var fundPct = grabValue("fundPct")/100;
-var payPeriod = grabValue("payPeriod");
-var retireAge = grabValue("retireAge");
-var deathAge = grabValue("deathAge");
-var returnPct = grabValue("returnPct")/100;
-var currentAge = grabValue("currentAge");
-var spend = grabValue("spend");
-var raise = grabValue("raise")/100;
-var initial = grabValue("initial");
-
-//Calculate Initial Values
-var yearlySave = income * fundPct;
-var paycheckSave = yearlySave / payPeriod;
-var monthlyPeriods = payPeriod / 12;
-var monthlySave = paycheckSave * monthlyPeriods;
-var monthlyPct = returnPct / 12;
-var monthsToRetire = (retireAge - currentAge) * 12;
-
-
 //Calculate Retirement
-
 function retireAnalysis() {
-    for (var i=1; i <= monthsToRetire; i++ ) {
 
+    //Grab Values from User Inputs
+    var income = grabValue("income");
+    var fundPct = grabValue("fundPct")/100;
+    var payPeriod = grabValue("payPeriod");
+    var retireAge = grabValue("retireAge");
+    var deathAge = grabValue("deathAge");
+    var returnPct = grabValue("returnPct")/100;
+    var currentAge = grabValue("currentAge");
+    var spend = grabValue("spend");
+    var raise = grabValue("raise")/100;
+    var initial = grabValue("initial");
+
+    //Calculate Initial Values
+    var yearlySave = income * fundPct;
+    var paycheckSave = yearlySave / payPeriod;
+    var monthlyPeriods = payPeriod / 12;
+    var monthlySave = paycheckSave * monthlyPeriods;
+    var monthlyPct = returnPct / 12;
+    var monthsToRetire = (retireAge - currentAge) * 12;
+    
+    var calc = initial;
+    var monthlyAdd = monthlySave;
+
+    for (var i=1; i <= monthsToRetire; i++ ) {     
+
+        //Calculate Monthly Increase - Every 12 Months Increase Account For Raise
         if ((i/12 - Math.floor(i/12)) === 0) {
 
-            
+            monthlyAdd = monthlyAdd * (1 + raise);
+            calc = (calc + monthlyAdd) * (1 + monthlyPct);
 
         } else {
 
-
+            calc = (calc + monthlyAdd) * (1 + monthlyPct);
         };
 
     };
+
+ retireValue = Math.round(calc);
 };
 
 //Get and Listen For Button Clicks
